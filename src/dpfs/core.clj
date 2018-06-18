2436ad5dc9571aee0027fe4194814eed32c2280a
CJ Apel <cjapel@liveramp.com>
2018-06-17 21:43:26 -0700

    add pretty print confirmation

--- src/dpfs/core.clj
+++ src/dpfs/core.clj
@@ -3,6 +3,10 @@
             [clojure.data.json :as json])
   (:gen-class :main true))
 
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
 (def config-path (str (System/getProperty "user.home") "/.docker/config.json"))
 
 (def valid-columns #{"ID"
@@ -26,6 +30,16 @@
 (defn go-template [s]
   (str "{{" "." s "}}" "\\t"))
 
+(defn pretty-format [s]
+  "From string of form \"table {{.Names}}\\t{{.Image}}\" to \" <Names> <Image>\""
+  ;; drop 'table' by splitting on space
+  (let [templated-cols-str (apply str (drop 1 (string/split s #" ")))]
+    (->> (string/split templated-cols-str #"\.")
+       (map #(re-find #"\w+" %))
+       (filter identity)
+       (map #(str "<" % ">"))
+       (string/join " "))))
+
 (defn get-fmt-string [& cols]
   "args should be column headers (e.g. Names, Image), outputs a psFormat table string"
   (string/join (concat ["table "] (map go-template cols))))
@@ -34,7 +48,8 @@
   (let [config (json/read-str (slurp config-path) :key-fn str)]
     (spit 
       config-path 
-      (json/write-str (conj config {"psFormat" s})))))
+      (json/write-str (conj config {"psFormat" s})))
+    (exit 0 (str "Updated ps format: " (pretty-format s)))))
 
 (defn help []
     (->> ["dpsf is a command line tool to edit how `docker ps` output is formatted"
@@ -48,10 +63,6 @@
           (str "dpsf " (name (first (keys style-to-columns))))
           "dpsf ID Names Labels"]
          (string/join \newline)))
-
-(defn exit [status msg]
-  (println msg)
-  (System/exit status))
 
 (defn valid-column? [col]
   (valid-columns (str col)))
