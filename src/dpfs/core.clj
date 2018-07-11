d48a5197e937a9a3fac550b36c00fef7e82d07f4
CJ Apel <cjapel@liveramp.com>
2018-07-11 00:34:35 -0700

    small refactor

--- src/dpfs/core.clj
+++ src/dpfs/core.clj
@@ -9,47 +9,12 @@
 
 (def config-path (str (System/getProperty "user.home") "/.docker/config.json"))
 
-(def valid-columns #{"ID"
-                     "Image"
-                     "Command"
-                     "CreatedAt"
-                     "RunningFor"
-                     "Status"
-                     "Ports"
-                     "Size"
-                     "Labels"
-                     "Label"
-                     "Mounts"
-                     "Networks"
-                     "Names"})
+(def valid-columns #{"ID" "Image" "Command" "CreatedAt" "RunningFor" "Status"
+                     "Ports" "Size" "Labels" "Label" "Mounts" "Networks" "Names"})
 
 (def style-to-columns 
   {:default ["ID" "Image" "Command" "CreatedAt" "Status" "Ports" "Names"]
     :short ["Names" "Image" "Status"]})
-
-(defn go-template [s]
-  (str "{{" "." s "}}" "\\t"))
-
-(defn pretty-format [s]
-  "From string of form \"table {{.Names}}\\t{{.Image}}\" to \" <Names> <Image>\""
-  ;; drop 'table' by splitting on space
-  (let [templated-cols-str (apply str (drop 1 (string/split s #" ")))]
-    (->> (string/split templated-cols-str #"\.")
-       (map #(re-find #"\w+" %))
-       (filter identity)
-       (map #(str "<" % ">"))
-       (string/join " "))))
-
-(defn get-fmt-string [& cols]
-  "args should be column headers (e.g. Names, Image), outputs a psFormat table string"
-  (string/join (concat ["table "] (map go-template cols))))
-
-(defn write-ps-format [s]
-  (let [config (json/read-str (slurp config-path) :key-fn str)]
-    (spit 
-      config-path 
-      (json/write-str (conj config {"psFormat" s})))
-    (exit 0 (str "Updated ps format: " (pretty-format s)))))
 
 (defn help []
     (->> ["dpsf is a command line tool to edit how `docker ps` output is formatted"
@@ -63,6 +28,29 @@
           (str "dpsf " (name (first (keys style-to-columns))))
           "dpsf ID Names Labels"]
          (string/join \newline)))
+
+(defn go-template [s]
+  (str "{{" "." s "}}" "\\t"))
+
+(defn pretty-format [s]
+  "From string of form \"table {{.Names}}\\t{{.Image}}\" to \" <Names> <Image>\""
+  ;; drop 'table' by splitting on space
+  (let [templated-cols-str (apply str (drop 1 (string/split s #" ")))]
+    (->> (string/split templated-cols-str #"\.")
+       (filter #(re-find #"\w+" %))
+       (map #(str "<" % ">"))
+       (string/join " "))))
+
+(defn get-fmt-string [& cols]
+  "args should be column headers (e.g. Names, Image), outputs a psFormat table string"
+  (string/join (concat ["table "] (map go-template cols))))
+
+(defn write-ps-format [s]
+  (let [config (json/read-str (slurp config-path) :key-fn str)]
+    (spit 
+      config-path 
+      (json/write-str (conj config {"psFormat" s})))
+    (exit 0 (str "Updated ps format: " (pretty-format s)))))
 
 (defn valid-column? [col]
   (valid-columns (str col)))
